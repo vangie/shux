5b853f2d6fb0c582e5f82ac0ea8d1d6d02225d30
James Halliday <mail@substack.net>
2013-02-25 04:13:33 -0800

    using request

--- example/client.js
+++ example/client.js
@@ -1,46 +1,32 @@
 var http = require('http');
 var through = require('through');
-var duplexer = require('duplexer');
+var request = require('request');
 
-var href = (process.argv[2] ? '/attach/' + process.argv[2] : '/open')
+var href = 'http://localhost:5000/'
+    + (process.argv[2] ? 'attach/' + process.argv[2] : 'open')
     + '?columns=' + process.stdout.columns + '&rows=' + process.stdout.rows
 ;
-var r = http.request({
-    method: 'POST',
-    host: 'localhost',
-    port: 5000,
-    path: href
-});
-r.setTimeout(0);
-r.on('response', function (res) { res.pipe(stdout) });
+var r = request.post(href);
+r.on('close', function () { process.exit() });
 
-var stdout = through();
-var stdin = through(function (buf) {
+var state = { meta: false };
+process.stdin.setRawMode(true);
+process.stdin.pipe(through(function (buf) {
     if (buf.length === 1 && buf[0] === 1) {
         state.meta = true;
     }
     else {
         if (state.meta && buf[0] === 'd'.charCodeAt(0)) {
             this.queue(null);
-            c.destroy();
+            process.exit();
         }
         else {
             this.queue(buf);
         }
         state.meta = false;
     }
-});
+})).pipe(r).pipe(process.stdout);
 
-var c = duplexer(r, stdout);
-stdin.pipe(c);
-c.pipe(process.stdout);
-c.on('close', function () { process.exit() });
-
-var state = { meta: false };
-process.stdin.pipe(stdin);
-process.stdin.resume();
-
-process.stdin.setRawMode(true);
 process.on('exit', function () {
     process.stdin.setRawMode(false);
     console.log();
