bfe925f67f8e51a86ea8db4f2512ab8af4434827
James Halliday <mail@substack.net>
2013-02-25 04:51:07 -0800

    add the keys

--- example/drone.js
+++
@@ -1,8 +0,0 @@
-var net = require('net');
-var rshell = require('../../');
-var rsh = rshell(require('./keys/drone.json'));
-
-var stream = net.connect(5000, 'localhost');
-var dr = rsh.createDrone(require('./keys/authorized.json'));
-
-dr.pipe(stream).pipe(dr);
